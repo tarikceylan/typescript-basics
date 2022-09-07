5fc1da276393325d8a513fa62681fde261214d8b
tarikceylan <tarikceylan@outlook.com>
2022-09-07 15:32:37 +0300

    Update unions.ts

--- dist/unions.js
+++ dist/unions.js
@@ -1,3 +1,8 @@
+/* If using unions, it's not possible to use a method specific to one of the types
+For Ex; We can not use .toExponential() method on MyType
+*/
+var myNumber = 10;
+myNumber.toExponential(2);
 //myValue has a boolean type
 var myValue = false;
 console.log(myValue);
@@ -8,7 +13,4 @@ var myState = 'Idle';
 console.log(myState);
 //TypeScript won't throw an error since 'Working' is a possible value. 
 myState = 'Working'; //But it's case sensitive. The string 'working' will throw an error
-console.log(myState);
-//TypeScript will throw en error since 'Bored' is not a possible value
-myState = 'Bored';
 console.log(myState);
